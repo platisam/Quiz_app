75b4a924a2620f4a8435cbdb0edb54471fbd2a98
platisam <platisam@gmail.com>
2023-05-22 13:16:45 +0200

    adding styles to quiz-container and spans

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -8,7 +8,7 @@ const Quiz = ({ questions }) => {
   return (
     <div className="quiz-container">
       <>
-        <span className="active-question -no">{currentQuestion + 1}</span>
+        <span className="active-question-no">{currentQuestion + 1}</span>
         <span className="total-question">/{questions.length}</span>
       </>
     </div>
