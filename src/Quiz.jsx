1f385a9256966a9b19667c9548acbfbaed1f9bcc
platisam <platisam@gmail.com>
2023-05-23 13:15:19 +0200

    showing results with onTryAgain reset button

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -42,6 +42,11 @@ const Quiz = ({ questions }) => {
     }
   };
 
+  const onTryAgain = () => {
+    setResult(resultInitialState);
+    setShowResult(false);
+  };
+
   return (
     <div className="quiz-container">
       {!showResult ? (
@@ -69,6 +74,19 @@ const Quiz = ({ questions }) => {
       ) : (
         <div className="result">
           <h3>Result</h3>
+          <p>
+            Total Questions: <span>{questions.length}</span>
+          </p>
+          <p>
+            Total Score: <span>{result.score}</span>
+          </p>
+          <p>
+            Correct Answers: <span>{result.correctAnswers}</span>
+          </p>
+          <p>
+            Wrong Answers: <span>{result.wrongAnswers}</span>
+          </p>
+          <button onClick={onTryAgain}>Try Again</button>
         </div>
       )}
     </div>
