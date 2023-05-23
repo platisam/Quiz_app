55897153c5ff305429cc9b2ecc892594e0820642
platisam <platisam@gmail.com>
2023-05-23 12:50:45 +0200

    added showResult state with conditional rendering for result

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -6,6 +6,7 @@ const Quiz = ({ questions }) => {
   const [answerIdx, setAnswerIdx] = useState(null);
   const [answer, setAnswer] = useState(null);
   const [result, setResult] = useState(resultInitialState);
+  const [showResult, setShowResult] = useState(false);
 
   const { question, choices, correctAnswer } = questions[currentQuestion];
 
@@ -37,32 +38,39 @@ const Quiz = ({ questions }) => {
       setCurrentQuestion((prev) => prev + 1);
     } else {
       setCurrentQuestion(0);
+      setShowResult(true);
     }
   };
 
   return (
     <div className="quiz-container">
-      <>
-        <span className="active-question-no">{currentQuestion + 1}</span>
-        <span className="total-question">/{questions.length}</span>
-        <h2>{question}</h2>
-        <ul>
-          {choices.map((choice, index) => (
-            <li
-              onClick={() => onAnswerClick(choice, index)}
-              key={choice}
-              className={answerIdx === index ? "selected-answer" : null}
-            >
-              {choice}
-            </li>
-          ))}
-        </ul>
-        <div className="footer">
-          <button onClick={onClickNext} disabled={answerIdx === null}>
-            {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
-          </button>
+      {!showResult ? (
+        <>
+          <span className="active-question-no">{currentQuestion + 1}</span>
+          <span className="total-question">/{questions.length}</span>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((choice, index) => (
+              <li
+                onClick={() => onAnswerClick(choice, index)}
+                key={choice}
+                className={answerIdx === index ? "selected-answer" : null}
+              >
+                {choice}
+              </li>
+            ))}
+          </ul>
+          <div className="footer">
+            <button onClick={onClickNext} disabled={answerIdx === null}>
+              {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="result">
+          <h3>Result</h3>
         </div>
-      </>
+      )}
     </div>
   );
 };
