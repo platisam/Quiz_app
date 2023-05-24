9cf8f0f58587689142fb72487a3ed48982cfbeec
platisam <platisam@gmail.com>
2023-05-24 15:40:18 +0200

    creating a folder structure

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { resultInitialState } from "./constants";
+import { resultInitialState } from "../../constants";
+import "./Quiz.scss";
 
 const Quiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
