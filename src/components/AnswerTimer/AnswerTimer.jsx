36a09218e259eb2809100968637e7a645024436d
platisam <platisam@gmail.com>
2023-05-24 16:37:30 +0200

    added useEffect hook with setInterval for the counter

--- src/components/AnswerTimer/AnswerTimer.jsx
+++ src/components/AnswerTimer/AnswerTimer.jsx
@@ -1,6 +1,14 @@
 import "./AnswerTimer.scss";
+import { useEffect, useState } from "react";
 
 const AnswerTimer = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCounter((cur) => cur + 1);
+    }, 1000);
+  }, []);
   return (
     <div className="answer-timer-container">
       <div className="progress"></div>
