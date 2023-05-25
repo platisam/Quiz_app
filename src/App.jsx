d74fca2a1bcc15726c8962ad999e3b1693e855be
platisam <platisam@gmail.com>
2023-05-25 22:15:40 +0200

    storing questions in a variable and pass them to Quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Quiz from "./components/Quiz/Quiz";
-import { jsQuizz } from "./constants";
 import { useEffect, useState } from "react";
 
 function App() {
+  const [questions, setQuestion] = useState([]);
+
   useEffect(() => {
     getQuestions();
   }, []);
@@ -12,14 +13,15 @@ function App() {
       const response = await fetch(
         "https://644982a3e7eb3378ca4ba471.mockapi.io/questions"
       );
-      const questionsRespons = await response.json();
-      console.log(questionsRespons);
+      const questionsResponse = await response.json();
+      console.log(questionsResponse);
+      setQuestion(questionsResponse);
     } catch (error) {
       console.log(error);
     }
   };
 
-  return <Quiz questions={jsQuizz.questions} />;
+  return questions.length && <Quiz questions={questions} />;
 }
 
 export default App;
